6aec49aa6767d04bcd962990af8729429238b3ff
xgao@zoyoe.com <xgao@zoyoe.com>
2020-11-14 19:03:52 +0800

    add webpack to postinstall

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 console.log("test");
 var demo = require ("../lib/js/src/demo.bs.js");
-var parent = document.getElementById("test");
-demo.demo_cfg(parent)
+var parent = document.getElementById("test-svg-helper");
+if (parent) {
+  demo.demo_cfg(parent)
+}
+
+module.exports = {
+  demo: demo,
+}
